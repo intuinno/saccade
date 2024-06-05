c6cff1989e30b304fb81ea204cba7c12894a565d
Deokgun Park <intuinno@gmail.com>
2024-06-04 23:31:26 -0500

    test vector environment working

--- envs/vec_sac_env.py
+++ envs/vec_sac_env.py
@@ -11,14 +11,14 @@ import torchvision
 torch._dynamo.config.capture_func_transforms = True
 
 
-class SaccadeEnv(gym.Env):
+class VecSaccadeEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 5}
 
     def __init__(
         self,
         central_size=16,
         peri_size=8,
-        num_loc_per_side=7,
+        num_loc_per_side=4,
         max_speed=2,
         seq_len=100,
         device="cpu",
@@ -26,7 +26,8 @@ class SaccadeEnv(gym.Env):
         num_environment=10,
     ):
         self.width, self.height = 64, 64  # The size of the mmnist image
-        self.window_size = 108
+        self.window_width = 300
+        self.window_height = 108
         self.mnist_width, self.mnist_height = 28, 28  # The size of the mnist patch
         self.central_size = central_size
         self.peri_size = peri_size
@@ -46,7 +47,7 @@ class SaccadeEnv(gym.Env):
 
         mnist = torchvision.datasets.MNIST("datasets", download=True)
         self.mnist = torch.tensor(mnist.data.numpy(), device=device)
-        self.num_patches = mnist.shape[0]
+        self.num_patches = self.mnist.shape[0]
 
         self.lims = (
             self.width - self.mnist_width,
@@ -73,6 +74,7 @@ class SaccadeEnv(gym.Env):
         self.render_mode = render_mode
         self.window = None
         self.clock = None
+        self._vmap_build_canvas = torch.vmap(self._build_canvas)
 
     def reset(self, seed=None):
         # super().reset()
@@ -86,17 +88,40 @@ class SaccadeEnv(gym.Env):
         return self.observation, self.info
 
     def _reset(self):
-        direcs = math.pi * (torch.rand((self.nums_per_image,)) * 2 - 1)
-        indexes = np.random.randint(self.num_images, size=(self.nums_per_image))
-        self.patches = torch.tensor(self.images[indexes])
-        speeds = np.random.randint(0, self.max_speed, size=(self.nums_per_image,))
-        self.velocs = torch.tensor(
-            [(s * torch.cos(d), s * torch.sin(d)) for d, s in zip(direcs, speeds)]
+        direcs = math.pi * (
+            torch.rand(
+                (
+                    self.num_env,
+                    self.nums_per_image,
+                ),
+                device=self.device,
+            )
+            * 2
+            - 1
         )
-        self.loc = torch.randint(self.num_loc_per_side**2, (1,))[0]
+        self.indexes = torch.randint(
+            self.num_patches,
+            size=(self.num_env, self.nums_per_image),
+            device=self.device,
+        )
+        speeds = torch.randint(
+            0,
+            self.max_speed,
+            size=(
+                self.num_env,
+                self.nums_per_image,
+            ),
+            device=self.device,
+        )
+        self.velocs = torch.stack(
+            (speeds * torch.cos(direcs), speeds * torch.sin(direcs)),
+            dim=1,
+        )
+        self.loc = torch.randint(self.num_loc_per_side**2, (self.num_env,))
         self.positions = torch.mul(
             torch.rand(
                 (
+                    self.num_env,
                     self.nums_per_image,
                     2,
                 )
@@ -106,25 +131,43 @@ class SaccadeEnv(gym.Env):
         self.step_count = 0
 
     def _get_obsv(self):
-        canvas = torch.zeros(self.width, self.height)
-        for patch, pos in zip(self.patches, self.positions):
-            canvas += self._build_canvas(patch, pos)
+        canvas = torch.zeros(self.num_env, self.width, self.height)
+
+        for i in range(self.nums_per_image):
+            buf = [
+                self._build_canvas(patch, pos)
+                for patch, pos in zip(
+                    self.mnist[self.indexes[:, i]], self.positions[:, i, :]
+                )
+            ]
+            buf = torch.stack(buf)
+
+            canvas += buf
         canvas = torch.where(canvas > 255.0, 255.0, canvas)
         self.canvas = canvas
 
-        x, y = self._get_loc_coord()
+        xs, ys = self._get_loc_coord()
 
-        self.central_vision = canvas[
-            x : x + self.central_size, y : y + self.central_size
-        ]
-
-        self.peri_vision = einops.reduce(
-            canvas,
-            "(w1 w2) (h1 h2) -> w1 h1",
-            "mean",
-            w1=self.peri_size,
-            h1=self.peri_size,
+        self.central_vision = torch.stack(
+            [
+                c[x : x + self.central_size, y : y + self.central_size]
+                for c, x, y in zip(self.canvas, xs, ys)
+            ]
         )
+
+        self.peri_vision = torch.stack(
+            [
+                einops.reduce(
+                    c,
+                    "(w1 w2) (h1 h2) -> w1 h1",
+                    "mean",
+                    w1=self.peri_size,
+                    h1=self.peri_size,
+                )
+                for c in self.canvas
+            ]
+        )
+
         observation = {
             "central": self.central_vision,
             "peripheral": self.peri_vision,
@@ -147,7 +190,7 @@ class SaccadeEnv(gym.Env):
         canvas[x : x + self.mnist_width, y : y + self.mnist_height] = patch
         return canvas
 
-    def step(self, action):
+    def step(self, actions):
         next_pos = self.positions + self.velocs
         self.velocs = torch.where(
             ((next_pos < -2) | (next_pos > self.lims + 1)),
@@ -163,7 +206,7 @@ class SaccadeEnv(gym.Env):
         else:
             done = False
 
-        self.loc = action
+        self.loc = actions
         self.observation, self.info = self._get_obsv()
 
         if self.render_mode == "human":
@@ -190,40 +233,21 @@ class SaccadeEnv(gym.Env):
             pygame.init()
             pygame.display.init()
             self.window = pygame.display.set_mode(
-                (self.window_size, self.window_size), HWSURFACE | DOUBLEBUF | RESIZABLE
+                (self.window_width, self.window_height),
+                HWSURFACE | DOUBLEBUF | RESIZABLE,
             )
             self.draw_screen = self.window.copy()
         if self.clock is None and self.render_mode in ["human", "rgb_array"]:
             self.clock = pygame.time.Clock()
 
-        canvas = pygame.Surface((self.window_size, self.window_size))
+        # canvas = pygame.Surface((self.window_width, self.window_height))
         self.draw_screen.fill((100, 100, 100))
         pygame.display.flip()
 
-        image = self.canvas.numpy()
-        surf = self.get_surface(image)
-        self.draw_screen.blit(surf, (0, 0))
-
-        peri = self.peri_vision.numpy()
-        surf = self.get_surface(peri)
-        # self.draw_screen.blit(surf, (30 + self.box_side_length, 72))
-        self.draw_screen.blit(
-            pygame.transform.scale(surf, (self.central_size, self.central_size)),
-            (30 + self.central_size, 72),
-        )
-
-        central = self.central_vision.numpy()
-        surf = self.get_surface(central)
-        self.draw_screen.blit(surf, (10, 72))
-
-        # Draw red line around loc
-        top, left = self._get_loc_coord()
-        pygame.draw.rect(
-            self.draw_screen,
-            (255, 0, 0),
-            pygame.Rect(left, top, self.central_size, self.central_size),
-            width=1,
-        )
+        offset = 0
+        for i in range(3):
+            self.draw_env(i, offset)
+            offset += 100
 
         if self.render_mode == "human":
             self.window.blit(
@@ -237,6 +261,34 @@ class SaccadeEnv(gym.Env):
             return np.transpose(
                 np.array(pygame.surfarray.pixels3d(self.draw_screen)), axes=(1, 0, 2)
             )
+
+    def draw_env(self, index, offset):
+        image = self.canvas[index].numpy()
+        surf = self.get_surface(image)
+        self.draw_screen.blit(surf, (offset, 0))
+
+        peri = self.peri_vision[index].numpy()
+        surf = self.get_surface(peri)
+        # self.draw_screen.blit(surf, (30 + self.box_side_length, 72))
+        self.draw_screen.blit(
+            pygame.transform.scale(surf, (self.central_size, self.central_size)),
+            (30 + self.central_size + offset, 72),
+        )
+
+        central = self.central_vision[index].numpy()
+        surf = self.get_surface(central)
+        self.draw_screen.blit(surf, (10 + offset, 72))
+
+        # Draw red line around loc
+        top, left = self._get_loc_coord()
+        top = top[index]
+        left = left[index]
+        pygame.draw.rect(
+            self.draw_screen,
+            (255, 0, 0),
+            pygame.Rect(left + offset, top, self.central_size, self.central_size),
+            width=1,
+        )
 
     def close(self):
         if self.window is not None:
